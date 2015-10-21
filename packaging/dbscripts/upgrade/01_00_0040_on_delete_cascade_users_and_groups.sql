59564dd9693ac3f4e86d34bf3d92c96c20b963da
Ori Liel <oliel@redhat.com>
2015-10-21 12:55:07 +0300

    Allow removing non-empty groups.
    Until now it was impossible to delete a group which has users or other
    groups in it. The user would have to first manually sever all ties of
    this group to other users/groups.
    
    This patch enables deleting a non-emtpy groups. Memeberships in the
    group are cancelled implicitly.
    
    Bug-Url: http://bugzilla.redhat.com/1241883
    Change-Id: I08f2441600a19696c25a03f9cfed4fe39a449fa6
    Signed-off-by: Ori Liel <oliel@redhat.com>

---
+++ packaging/dbscripts/upgrade/01_00_0040_on_delete_cascade_users_and_groups.sql
@@ -0,0 +1,4 @@
+SELECT fn_db_drop_constraint('user_groups', 'user_groups_in_group_id_fkey');
+SELECT fn_db_create_constraint('user_groups', 'user_groups_in_group_id_fkey', 'FOREIGN KEY (in_group_id) REFERENCES aaa_jdbc.groups (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE');
+SELECT fn_db_drop_constraint('group_groups', 'group_groups_in_group_id_fkey');
+SELECT fn_db_create_constraint('group_groups', 'group_groups_in_group_id_fkey', 'FOREIGN KEY (in_group_id) REFERENCES aaa_jdbc.groups (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE');
