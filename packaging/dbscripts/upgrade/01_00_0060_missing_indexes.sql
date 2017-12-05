d23820c027a73bf31ec891064fe3d64bc2e2b184
Martin Perina <mperina@redhat.com>
2017-12-05 13:43:05 +0100

    Fix creation of missing indexes
    Fixes creation of missing indexes (introduced in change
    I5702fb29437e9e901829d982fd087410365998ba):
    
    1. To ensure they are created only once.
    2. To remove 'aaa_jdbc' schema pro upgrade file as correct schema name
    is used when upgrade scripts are execute (aaa-jdbc can be installed with
    different schema names).
    
    Change-Id: I8f03f0cbab0fc9d37147f1cbf61486f3b7ffd7a9
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- packaging/dbscripts/upgrade/01_00_0060_missing_indexes.sql
+++ packaging/dbscripts/upgrade/01_00_0060_missing_indexes.sql
@@ -1,8 +1,8 @@
-CREATE INDEX idx_user_groups_user_id ON aaa_jdbc.user_groups (user_id);
-CREATE INDEX idx_group_attributes_group_id ON aaa_jdbc.group_attributes (group_id);
-CREATE INDEX idx_user_groups_in_group_id ON aaa_jdbc.user_groups (in_group_id);
-CREATE INDEX idx_group_groups_group_id ON aaa_jdbc.group_groups (group_id);
-CREATE INDEX idx_user_password_history_user_id ON aaa_jdbc.user_password_history (user_id);
-CREATE INDEX idx_user_attributes_user_id ON aaa_jdbc.user_attributes (user_id);
-CREATE INDEX idx_group_groups_in_group_id ON aaa_jdbc.group_groups (in_group_id);
-CREATE INDEX idx_failed_logins_user_id ON aaa_jdbc.failed_logins (user_id);
+CREATE INDEX IF NOT EXISTS idx_user_groups_user_id ON user_groups (user_id);
+CREATE INDEX IF NOT EXISTS idx_group_attributes_group_id ON group_attributes (group_id);
+CREATE INDEX IF NOT EXISTS idx_user_groups_in_group_id ON user_groups (in_group_id);
+CREATE INDEX IF NOT EXISTS idx_group_groups_group_id ON group_groups (group_id);
+CREATE INDEX IF NOT EXISTS idx_user_password_history_user_id ON user_password_history (user_id);
+CREATE INDEX IF NOT EXISTS idx_user_attributes_user_id ON user_attributes (user_id);
+CREATE INDEX IF NOT EXISTS idx_group_groups_in_group_id ON group_groups (in_group_id);
+CREATE INDEX IF NOT EXISTS idx_failed_logins_user_id ON failed_logins (user_id);
