6ec4cbcd201348dcf5b24ba71f8f89c1e80eec78
Martin Perina <mperina@redhat.com>
2019-03-01 12:22:33 +0100

    core: Log tool execution to syslog
    Adds logs record of each ovirt-aaa-jdbc-tool execution into syslog. Each
    record contains:
    
     - Username of a user who executed the tool
     - Tool and command line parameters (password is filtered)
     - Result of tool execution
    
    Change-Id: Iae5862d6ecdb67f68ace2f3389a9ca20f39ff480
    Bug-Url: https://bugzilla.redhat.com/1619391
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- packaging/bin/ovirt-aaa-jdbc-tool.sh
+++ packaging/bin/ovirt-aaa-jdbc-tool.sh
@@ -2,7 +2,7 @@
 
 . "$(dirname "$(readlink -f "$0")")"/../../ovirt-engine/bin/engine-prolog.sh
 
-exec "${JAVA_HOME}/bin/java" \
+"${JAVA_HOME}/bin/java" \
 	-Djava.security.auth.login.config="${ENGINE_USR}/conf/jaas.conf" \
 	-Djava.util.logging.config.file="${OVIRT_LOGGING_PROPERTIES}" \
 	-Djboss.modules.write-indexes=false \
@@ -12,3 +12,9 @@ exec "${JAVA_HOME}/bin/java" \
 	-dependencies org.ovirt.engine.extension.aaa.jdbc \
 	-class org.ovirt.engine.extension.aaa.jdbc.binding.cli.Cli \
 	"$@"
+
+# Log message about execution
+rc=$?
+result=$([[ ${rc} == 0 ]] && echo -n "successfully." || echo -n "with failure.")
+params="$(echo "$@" | sed 's/password=pass\:[^[:space:]]*/password=pass\:*** /g')"
+logger "User '${USER}' executed '$0 ${params}' ${result}"
